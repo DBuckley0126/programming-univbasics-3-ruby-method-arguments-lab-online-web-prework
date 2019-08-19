a1a8e519bc2ff94771c0251ba7d1a798919d862a
Danny Buckley <dannyboy.msn@hotmail.com>
2019-08-19 17:22:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
+def introduction(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
 
 
 
